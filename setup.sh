e9cdde4bc87fe8be9dfad611e9dd75e2bb137cbb
Josh Errickson <jerrick@umich.edu>
2013-08-10 08:01:19 -0400

    Added xmonad to setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ ln -s ~/dotfiles/.aliases ~/.
 ln -s ~/dotfiles/.Rprofile ~/.
 ln -s ~/dotfiles/.zshrc ~/.
 ln -s ~/dotfiles/init.el ~/.emacs.d/
+ln -s ~/dotfiles/xmobar.rc ~/.xmonad/
+ln -s ~/dotfiles/xmonad.hs ~/.xmonad/
+ln -s ~/dotfiles/xmonad-init ~/.xmonad/
