85764b6996163d892e1d23c2617f23e68e7918f9
Josh Errickson <jerrick@umich.edu>
2013-08-12 10:13:38 -0400

    Cleaned up setup, esp with linux vs os x

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ostype=$(uname)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s "$DIR"/.aliases ~/.
@@ -7,14 +7,16 @@ ln -s "$DIR"/.Rprofile ~/.
 ln -s "$DIR"/.zshrc ~/.
 ln -s "$DIR"/.vimrc ~/.
 ln -s "$DIR"/.gtkrc-2.0 ~/.
-ln -s "$DIR"/.Xresources ~/.
-xrdb ~/.Xresources
+ln -s "$DIR"/init.el ~/.emacs.d/.
 
-ln -s "$DIR"/init.el ~/.emacs.d/
+if [[ $ostype == 'Linux' ]]; then
+    ln -s "$DIR"/.Xresources ~/.
+    xrdb ~/.Xresources
 
-ln -s "$DIR"/xmobar.rc ~/.xmonad/
-ln -s "$DIR"/xmonad.hs ~/.xmonad/
-ln -s "$DIR"/xmonad-init ~/.xmonad/
+    ln -s "$DIR"/xmobar.rc ~/.xmonad/.
+    ln -s "$DIR"/xmonad.hs ~/.xmonad/.
+    ln -s "$DIR"/xmonad-init ~/.xmonad/.
 
-ln -s "$DIR"/luakit/*.lua ~/.config/luakit/
-ln -s "$DIR"/luakit/adblock ~/.local/share/luakit/
+    ln -s "$DIR"/luakit/*.lua ~/.config/luakit/.
+    ln -s "$DIR"/luakit/adblock ~/.local/share/luakit/.
+fi
