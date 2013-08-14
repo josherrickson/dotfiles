4616c0b65045ac0958ee23787fa5d1c9fa1edb19
Josh Errickson <jerrick@umich.edu>
2013-08-14 10:26:00 -0400

    Changed luakit to link whole folder; first attempt at nicer setup script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
+source ~/.zshfunctions
 ostype=$(uname)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s "$DIR"/.aliases ~/.
-ln -s "$DIR"/.Rprofile ~/.
-ln -s "$DIR"/.zshrc ~/.
-ln -s "$DIR"/.zshfunctions ~/.
-ln -s "$DIR"/.vimrc ~/.
-ln -s "$DIR"/.gtkrc-2.0 ~/.
-ln -s "$DIR"/init.el ~/.emacs.d/.
+lns() {
+if [[ !(-a "$2") && (-h "$2") ]]; then
+    red invalid symlink "$2", replacing
+    rm "$2"
+    ln -s "$1" "$2"
+elif [[ (-a "$2") && ( -h "$2") ]]; then
+    yellow valid symlink "$2" exists, doing nothing
+elif [[ (-a "$2") && !( -h "$2") ]]; then
+    red "$2" is a real file, backing up and symlinking
+    mv "$2" "$2"_$(date '+%m.%d.%y_%H.%M.%S')
+    ln -s "$1" "$2"
+elif [[ !(-a "$2") && !( -h "$2") ]]; then
+    green no file, creating symlink
+    echo ln -s "$1" "$2"
+    ln -s "$1" "$2"
+fi
+}
+
+lns "$DIR"/.aliases ~/.aliases
+lns "$DIR"/.Rprofile ~/.Rprofile
+lns "$DIR"/.zshrc ~/.zshrc
+lns "$DIR"/.vimrc ~/.vimrc
+lns "$DIR"/.gtkrc-2.0 ~/.gtkrc-2.0
+lns "$DIR"/init.el ~/.emacs.d/init.el
 
 if [[ $ostype == 'Linux' ]]; then
-    ln -s "$DIR"/.Xresources ~/.
+    lns "$DIR"/.Xresources ~/.Xresources
     xrdb ~/.Xresources
 
-    ln -s "$DIR"/xmobar.rc ~/.xmonad/.
-    ln -s "$DIR"/xmonad.hs ~/.xmonad/.
-    ln -s "$DIR"/xmonad-init ~/.xmonad/.
+    lns "$DIR"/xmobar.rc ~/.xmonad/xmobar.rc
+    lns "$DIR"/xmonad.hs ~/.xmonad/xmonad.hs
+    lns "$DIR"/xmonad-init ~/.xmonad/xmonad-init
 
-    ln -s "$DIR"/luakit/*.lua ~/.config/luakit/.
-    ln -s "$DIR"/luakit/adblock ~/.local/share/luakit/.
+    lns "$DIR"/luakit/luakit ~/.config/luakit
+    lns "$DIR"/luakit/adblock ~/.local/share/luakit/adblock
 fi
