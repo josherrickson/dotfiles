413948289a750f1aae47904f00c70530f839991f
Josh Errickson <jerrick@umich.edu>
2013-08-24 07:19:41 -0400

    Emacs function to increment numbers

--- init.el
+++ init.el
@@ -169,6 +169,28 @@ goes to the true beginning of the line (before space.)"
     (setq exec-path (split-string path-from-shell path-separator))))
 (when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
 
+;; Run over a section to increment all numbers in the region
+;; http://jmdavisblog.blogspot.com/2013/08/a-handful-of-emacs-utilities.html
+(defun inc-num-region (p m)
+  "Increments the numbers in a given region"
+  (interactive "r")
+  (save-restriction
+    (save-excursion
+      (narrow-to-region p m)
+      (goto-char (point-min))
+      (forward-line)
+      (let ((counter 1))
+        (while (not (eq (point)
+                        (point-max)))
+          (goto-char (point-at-eol))
+          (search-backward-regexp "[0-9]+" (point-at-bol) t)
+          (let* ((this-num (string-to-number (match-string 0)))
+                 (new-num-str (number-to-string (+ this-num
+                                                   counter))))
+            (replace-match new-num-str)
+            (incf counter)
+            (forward-line)))))))
+
 ;;;;;;;;;;;;;;;
 ;;;;; ESS ;;;;;
 ;;;;;;;;;;;;;;;
