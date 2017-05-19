3dc89e9d1d518631acade4b10e2a925d827a2414
Josh Errickson <josh@errickson.net>
2017-05-19 15:20:46 -0400

    Removing battery from tmux on desktop

--- .tmux-scripts/battery.sh
+++ .tmux-scripts/battery.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-HEART='♥'
+islaptop=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Macbook")
 
-if [[ `uname` == 'Linux' ]]; then
-  current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
-  total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
-else
-  battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
-fi
+if [ "$islaptop" != '']; then
+    HEART='♥'
 
-charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 10 ]]; then
-  charged_slots=10
-fi
+     if [[ `uname` == 'Linux' ]]; then
+         current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
+         total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
+     else
+         battery_info=`ioreg -rc AppleSmartBattery`
+         current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+         total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+     fi
 
-echo -n '#[fg=red]'
-for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
+     charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
+     if [[ $charged_slots -gt 10 ]]; then
+         charged_slots=10
+     fi
 
-if [[ $charged_slots -lt 10 ]]; then
-  echo -n '#[fg=white]'
-  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n "$HEART"; done
+     echo -n '#[fg=red]'
+     for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
+
+     if [[ $charged_slots -lt 10 ]]; then
+         echo -n '#[fg=white]'
+         for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n "$HEART"; done
+     fi
 fi
