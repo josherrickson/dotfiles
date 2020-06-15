9fb2401d11da87b2f4cc85e84ed00e90cdeec038
Josh Errickson <josh@errickson.net>
2020-06-15 06:18:27 -0400

    A few fish change

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
+# No greeting
+set fish_greeting
+
+# PWD shouldn't shrink directory names
+set -g fish_prompt_pwd_dir_length 0
+
+
+
 alias grep='grep --color=always'
 alias fgrep='fgrep --color=always'
 alias tmux='tmux -2'
