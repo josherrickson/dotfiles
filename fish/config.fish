cf1b1e6927a85e8ac5115b153e9bae4ab280050a
Josh Errickson <josh@errickson.net>
2020-06-19 20:28:20 -0400

    Fixed bug with emacs color themes in fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,9 @@ set fish_greeting
 set -g fish_prompt_pwd_dir_length 0
 
 set -xg EDITOR emacs
+
+# For some reason, when using fish inside tmux in iTerm2, $TERM is "screen". This causes
+# emacs to think it doesn't have colors to properly display themes. (For some reason,
+# you need all three of those. fish in tmux in Terminal.app has proper $TERM, zsh in tmux
+# in iTerm2 has proper $TERM.)
+set -xg TERM xterm-256color
