0157945004c16f5a54fd6962e7ccda34f59523f9
Josh Errickson <josh@errickson.net>
2021-02-26 20:25:49 -0500

    ytdl shortcut

---
+++ fish/functions/ytdl.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function ytdl --wraps='youtube-dl -f bestvideo+bestaudio --write-sub --write-auto-sub --sub-format srt/best --sub-lang en --embed-subs' --description 'alias ytdl=youtube-dl -f bestvideo+bestaudio --write-sub --write-auto-sub --sub-format srt/best --sub-lang en --embed-subs'
+ command youtube-dl -f bestvideo+bestaudio --write-sub --write-auto-sub --sub-format srt/best --sub-lang en --embed-subs $argv;
+end
