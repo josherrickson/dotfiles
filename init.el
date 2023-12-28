514f417d5549bba14a07ad4c4e0cbdf55a4b8273
Josh Errickson <josh@errickson.net>
2023-12-28 14:50:29 -0500

    Lsp support

--- init.el
+++ init.el
@@ -666,6 +666,19 @@ Place the cursor on the '>' in '</span>'."
 (use-package rainbow-mode
   :hook (emacs-lisp-mode text-mode web-mode))
 
+;;;;;;;;;;;;;;;
+;;;;; lsp ;;;;;
+;;;;;;;;;;;;;;;
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; Custom file ;;;;;
